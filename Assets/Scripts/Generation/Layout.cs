118f2d742caabe1bfbcd55e2984fe00c59e72f23
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-09-09 16:34:44 +0200

    Started working on generator, but stopped due to some annoynig errors and lack of time

---
+++ Assets/Scripts/Generation/Layout.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layout {
+
+    // The rooms belonging to this layout
+    private List<Room> rooms; 
+
+    // Layout generator: 
+    public Layout(int numOfRooms)
+    {
+        List<Room> generatedRooms = new List<Room>();
+        Room firstRoom = new Room(this, 0, 0);
+        List<(int, int)> addedCoords = new List<(int, int)>();
+        //private (int,int) tuppledZeroZeroCoord = (0,0)
+        //addedCoords.add(tuppledZeroZeroCoord);
+
+        for (int i = 1; i < numOfRooms; i++) {
+            // We map the following numbers: 
+            // 0: move north
+            // 1: move east
+            // 2: move south
+            // 3: move west
+
+            int nextStep = Random.Range(0,4);
+        }
+
+        this.rooms = generatedRooms;
+    }
+
+   
+}
+
+
---
+++ Assets/Scripts/Generation/Room.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room
+{
+
+    // A room represents 1 block in our layout. A room is 1 scene. 
+    // When the player enters a room, unity will load in a new scene and place the player accordingly.  
+
+    // The layout this room belongs to
+    private Layout layout;
+
+    // The rooms have an X and Y coord, using these we will be able to generate the layouts 
+    private int XCord;
+    private int YCord;
+
+    public Room(Layout layout, int XCord, int YCord) {
+        this.layout = layout;
+        this.XCord = XCord;
+        this.YCord = YCord;
+    }
+
+
+
+
+
+}
