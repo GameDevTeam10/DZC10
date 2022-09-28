146341ab789b7d63616d8d37a40c337eae68fd45
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-09-28 14:54:20 +0200

    Started working on enemy AI. Added statemachine (is contained in Enemy class) and states. Next up, define state logic and figure out 2D pathfinding.

---
+++ Assets/Scripts/Characters/AI/States/IdleState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State {
+ 
+    public IdleState(Enemy enemy): base(enemy) {}
+
+    public override void stateStart() {
+     
+    }
+
+    public override void stateUpdate() {
+ 
+
+    }
+
+    public override void stateEnd() {
+
+    }
+}
