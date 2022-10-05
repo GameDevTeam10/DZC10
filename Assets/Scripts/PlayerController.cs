8fe58fbbe0771517ffca44b37dc61548d6cc21e4
Lucavdg <113116475+Lucavdg@users.noreply.github.com>
2022-10-05 22:12:24 +0200

    Progress character animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,29 +10,103 @@ public class PlayerController : MonoBehaviour
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
 
+    float inputHorizontal;
+    float inputVertical;
+
+
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator; 
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    string currentState;
+    const string PLAYER_IDLE = "Player_Idle";
+    const string PLAYER_WALK_LEFT = "Player_Walk_Left";
+    const string PLAYER_WALK_RIGHT = "Player_Walk_Right";
+    const string PLAYER_WALK_DOWN = "Player_Walk_Down";
+    const string PLAYER_WALK_UP = "Player_Walk_Up";
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Input
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate() {
-        if(movementInput != Vector2.zero) {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset);
-            if(count == 0) {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+        if (movementInput != Vector2.zero)
+        {
+            if (inputHorizontal > 0)
+            {
+                ChangeAnimationState(PLAYER_WALK_RIGHT);
             }
+            else if (inputHorizontal < 0)
+            {
+                ChangeAnimationState(PLAYER_WALK_LEFT);
+            }
+            else if (inputVertical > 0)
+            {
+                ChangeAnimationState(PLAYER_WALK_UP);
+            }
+            else if (inputVertical < 0)
+            {
+                ChangeAnimationState(PLAYER_WALK_DOWN);
+            }
+
+            
+
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+            }
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+            
         }
     }
 
+    private bool TryMove(Vector2 direction) {
+            int count = rb.Cast(
+                 direction,
+                 movementFilter,
+                 castCollisions,
+                 moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+       
+    }
     void OnMove(InputValue movementValue) {
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        //Stop animation from interupting itself
+        if (currentState == newState) return;
+
+        //Play new animation
+        animator.Play(newState);
+
+        //Update current state
+        currentState = newState;
+
     }
 }
